bdd3a02ca66bba21657030a05c16d371285dabf0
snecaj-netizen <snecaj@gmail.com>
2026-04-28 12:56:46 +0200

    feat: Implement PWA features for FloraWild
    Adds service worker registration, manifest file, and viewport meta tags to enable FloraWild as a Progressive Web App. This allows for offline caching and a more native-like installation experience.

---
+++ public/sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'florawild-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  'https://cdn-icons-png.flaticon.com/512/892/892926.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
